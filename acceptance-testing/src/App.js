791d500dcef3470763df7f770dd334c9bbe63d35
Rob Gietema <rob.gietema@gmail.com>
2017-06-14 16:59:23 +0200

    Setup editor build.

--- acceptance-testing/src/App.js
+++ acceptance-testing/src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
-import Editor from '../../src/components/Editor';
+import Editor from '../../lib/components/Editor';
 
 class App extends Component {
   render() {
-    return <div className="ui container"><h1>Draft.js Medium</h1></div>;
+    return (
+      <div className="ui container"><h1>Draft.js Medium</h1><Editor /></div>
+    );
   }
 }
 
