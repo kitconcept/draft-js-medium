490ca4207f96284bd7cdc750dfdd3877a613a44d
Rob Gietema <rob.gietema@gmail.com>
2017-06-14 17:16:44 +0200

    Added tooltip markup.

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -2,7 +2,24 @@ import React, { Component } from 'react';
 
 class Editor extends Component {
   render() {
-    return <h2>Editor</h2>;
+    return (
+      <div>
+        <div className="ui black pointing below label">
+          <div className="ui black buttons">
+            <button className="ui icon button">
+              <i aria-hidden="true" className="bold icon" />
+            </button>
+            <button className="ui icon button">
+              <i aria-hidden="true" className="italic icon" />
+            </button>
+            <button className="ui icon button">
+              <i aria-hidden="true" className="linkify icon" />
+            </button>
+          </div>
+        </div>
+        <h2>Editor</h2>
+      </div>
+    );
   }
 }
 
