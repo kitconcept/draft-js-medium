4e558a8273f4d17a8824f064d086ff6a04e21d55
Rob Gietema <rob.gietema@gmail.com>
2017-06-15 21:54:11 +0200

    Position the toolbar.

--- src/components/Toolbar/Toolbar.jsx
+++ src/components/Toolbar/Toolbar.jsx
@@ -4,6 +4,7 @@
  */
 
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
 
 import Button from '../Button/Button';
@@ -27,6 +28,30 @@ export default class Toolbar extends Component {
   };
 
   /**
+   * Component did update handler.
+   * @function componentDidUpdate
+   * @returns {undefined}
+   */
+  componentDidUpdate() {
+    if (!this.props.selection) {
+      return;
+    }
+
+    const selectionRect = window
+      .getSelection()
+      .getRangeAt(0)
+      .getBoundingClientRect();
+
+    const toolbarNode = ReactDOM.findDOMNode(this);
+    const toolbarRect = toolbarNode.getBoundingClientRect();
+
+    toolbarNode.style.top = `${selectionRect.top - toolbarRect.height - 6}px`;
+    toolbarNode.style.left = `${selectionRect.left +
+      selectionRect.width / 2 -
+      toolbarRect.width / 2}px`;
+  }
+
+  /**
    * Render method.
    * @function render
    * @returns {string} Markup of the container.
@@ -36,7 +61,10 @@ export default class Toolbar extends Component {
       return <span />;
     }
     return (
-      <div className="ui black pointing below label">
+      <div
+        className="ui black pointing below label"
+        style={{ position: 'absolute' }}
+      >
         <div className="ui black buttons">
           <StyleButton
             icon="bold"
